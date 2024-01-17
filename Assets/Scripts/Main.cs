f1e49e11e385eb4c12c9aba855fb2e51505331b2
onlymutalisk <onlymutalisk@gmail.com>
2024-01-17 18:54:16 +0900

    Fix: Repair Menu On/Off after Clear

--- Assets/Scripts/Main.cs
+++ Assets/Scripts/Main.cs
@@ -4,6 +4,8 @@ public class Main : MonoBehaviour
 {
     public void Awake()
     {
+        GameObject gameObjectMenu = GameObject.Find("CanvasDontDestroy").transform.GetChild(0).gameObject;
+        gameObjectMenu.SetActive(false);
         GameManager.stages.Clear();
     }
 }
