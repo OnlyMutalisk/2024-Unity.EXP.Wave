97d6a5946c309f20d24adcc7d638dcd41d2c3fae
jebinary <155892698+jebinary@users.noreply.github.com>
2024-01-20 09:48:15 +0900

    Fix: Repair PlayerHandler Scripts
    공중에서 쉬프트를 누른상태로 바닥에 도착하면 바로 달림
    
    왼쪽과 오른쪽 화살표 키보드를 같이 누르는 상황을 정지로 처리함, 불빛도 생성하지 않음
    
    변수 이름 수정 (isJump -> jumpable, foot -> Stomp)
    
    추후 세부사항에 맞게 더 보강 예정

--- Assets/Scripts/Script/Stage2_Script/PlayerHandler.cs
+++ Assets/Scripts/Script/Stage2_Script/PlayerHandler.cs
@@ -6,11 +6,14 @@ public class PlayerHandler : MonoBehaviour
 {
     Rigidbody2D rig;
     // 시간관련 타이머 설정
-    public UnityEngine.UI.Text text_Timer; // 쿨타임계산
+    public UnityEngine.UI.Text text_Timer;
+    // 쿨타임계산
     private float time_current;
-    private float time_Max = 2.8f;
-    private bool isEnded_foot = true;
-    private bool isFooting = false;
+    private float cooltime_Stomp = 2.8f;
+    private float cooltime_Shout = 2.8f;
+    private bool isEnded_Stomp = true;
+    private bool isStomping = false;
+    private bool isShouting = false;
     private bool isWalking = false;
 
     // 움직임 관련 정의
@@ -18,7 +21,7 @@ public class PlayerHandler : MonoBehaviour
     public float maxSpeed;
     public float jumpPower;
     public float Scale = 3f; // 캐릭터 크기조정
-    public bool isJump = false; 
+    public bool jumpable = false; 
     float First_moveSpeed = 3; // 
     float First_maxSpeed = 5;   
     float Run_moveSpeed = 6;
@@ -29,71 +32,111 @@ public class PlayerHandler : MonoBehaviour
     //분신 관련 정의
     public GhostLightHandler ghost;
 
-    private void Start()
+    // Walk 함수에 필요한 bool 값
+    bool isTwoArrow = false;
+    bool isDelayingInput = false;
+    void Walk()
     {
-        ghost = this.gameObject.GetComponent<GhostLightHandler>();
-        rig = GetComponent<Rigidbody2D>();
-        moveSpeed = First_moveSpeed;
-        maxSpeed = First_maxSpeed;
-        jumpPower = 14;
-}
-    void Update()
-    {
+        // 딜레이 중인 동안 입력되는 인풋은 무시
+        if (isDelayingInput) 
+        { 
+            return; 
+        }
+        // 두 키 중 하나라도 누르고 있지 않은 경우 isTwoArrow를 false로 만듬 
+        if(!Input.GetKey(KeyCode.LeftArrow) || !Input.GetKey(KeyCode.RightArrow))
+        {
+            isTwoArrow = false;
+        }
+        // 두 키를 동시에 누르고 있는 상태(isTwoArrow가 true)면 밑의 함수를 실행하지 않음(인풋 무시)
+        if (isTwoArrow)
+        {
+            return;
+        }
+        // 플레이어가 두 키를 동시에 누르고 있다가 거의 동시에(Delaying의 시간내에) 땠을 때 조금이라도 늦게 땐 쪽의 인풋을 무시하기 위함
+        if((Input.GetKey(KeyCode.LeftArrow) && Input.GetKeyUp(KeyCode.RightArrow)) ||
+          (Input.GetKey(KeyCode.RightArrow) && Input.GetKeyUp(KeyCode.LeftArrow)))
+        {
+            isDelayingInput = true;
+            StartCoroutine(Delaying());
+            return;
+        }
         // 왼쪽 화살표를 누르고 있는 경우
         if (Input.GetKey(KeyCode.LeftArrow))
         {
             // 달리는 것 인지
             isWalking = true;
-
-            // 잔상 불빛을 만듭니다.
-            ghost.makeGhost = true; 
-
-            // 물체에 왼쪽 방향으로 물리적 힘을 가해줍니다. 즉, 왼쪽으로 이동 시킵니다.
-            rig.AddForce(Vector2.left * moveSpeed, ForceMode2D.Impulse);
-
-            // velocity 는 물체의 속도입니다. 일정 속도에 도달하면 더 이상 빨라지지 않게합니다.
-            rig.velocity = new Vector2(Mathf.Max(rig.velocity.x, -maxSpeed), rig.velocity.y);
-
             // scale 값을 이용해 캐릭터가 이동 방향을 바라보게 합니다.
             transform.localScale = new Vector3(-Scale, Scale, Scale);
-        }
-        else if (Input.GetKey(KeyCode.RightArrow)) // 오른쪽 화살표를 누르고 있는 경우
-        {
-            // 달리는 것 인지
-            isWalking = true;
-
-            // 잔상 불빛을 만듭니다
+            // 잔상 불빛을 만듭니다.
             ghost.makeGhost = true;
-
+            // 물체에 왼쪽 방향으로 물리적 힘을 가해줍니다. 즉, 왼쪽으로 이동 시킵니다.
+            rig.AddForce(Vector2.left * moveSpeed, ForceMode2D.Impulse);
+            // velocity 는 물체의 속도입니다. 일정 속도에 도달하면 더 이상 빨라지지 않게합니다.
+            rig.velocity = new Vector2(Mathf.Max(rig.velocity.x, -maxSpeed), rig.velocity.y);
+        }
+        // 왼쪽 화살표를 누르는 중에 오른쪽 화살표가 눌린 경우
+        if (Input.GetKey(KeyCode.LeftArrow) && Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            // 정지한 것으로 처리함
+            isWalking = false;
+            ghost.makeGhost = false;
+            rig.velocity = new Vector3(rig.velocity.normalized.x, rig.velocity.y);
+            // 두 키가 동시에 눌리고 있는 상태라 명시
+            isTwoArrow = true;
+            // 인풋을 무시하는 상태로 만듬
+            isDelayingInput = true;
+            // 일정 시간 기다렸다가 인풋을 다시 받는 상태로 돌림
+            StartCoroutine(Delaying());
+            // 밑의 함수를 실행하지 않음
+            return;
+        }
+        // 오른쪽 화살표를 누르고 있는 경우
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            isWalking = true;
+            transform.localScale = new Vector3(Scale, Scale, Scale);
+            ghost.makeGhost = true;
             rig.AddForce(Vector2.right * moveSpeed, ForceMode2D.Impulse);
             rig.velocity = new Vector2(Mathf.Min(rig.velocity.x, maxSpeed), rig.velocity.y);
-            transform.localScale = new Vector3(Scale, Scale, Scale);
         }
-        else if (Input.GetKeyUp(KeyCode.LeftArrow) || Input.GetKeyUp(KeyCode.RightArrow)) // 이동 키를 뗀 경우
+        // 오른쪽 화살표를 누르는 중에 왼쪽 화살표가 눌린 경우
+        if (Input.GetKey(KeyCode.RightArrow) && Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            isWalking = false;
+            ghost.makeGhost = false;
+            rig.velocity = new Vector3(rig.velocity.normalized.x, rig.velocity.y);
+            isTwoArrow = true;
+            isDelayingInput = true;
+            StartCoroutine(Delaying());
+            return;
+        }
+        // 이동 키를 뗀 경우
+        if (Input.GetKeyUp(KeyCode.LeftArrow) || Input.GetKeyUp(KeyCode.RightArrow))
         {
             // 멈춘것 인지
             isWalking = false;
 
             // 이동키를 떼면 잔상을 만들지않습니다.
             ghost.makeGhost = false;
-            
+
             // x 속도를 줄여 이동 방향으로 아주 살짝만 움직이고 거의 바로 멈추게 합니다.
             rig.velocity = new Vector3(rig.velocity.normalized.x, rig.velocity.y);
+            isDelayingInput = true;
+            StartCoroutine(Delaying());
         }
-
-
-
-        // 스페이스바를 누르면 점프합니다.
-        if (Input.GetKeyDown(KeyCode.Space))
-        {
-            if (isJump)
-                rig.AddForce(Vector2.up * jumpPower, ForceMode2D.Impulse);
-        }
-        
+    }
+    IEnumerator Delaying()
+    {
+        yield return new WaitForSeconds(0.10f);
+        // 위 시간만큼 기다린 다음 인풋을 다시 받는 상태로 만듬
+        isDelayingInput = false;
+        Debug.Log("딜레이 해제");
+    }
+    void Dash()
+    {
         // 쉬프트를 누르면 달립니다. 최대 속도가 올라가고 가속도가 증가합니다. 바닥에서만 가능합니다.
-        if ((Input.GetKeyDown(KeyCode.LeftShift)||Input.GetKeyDown(KeyCode.RightShift))&&isJump)
+        if ((Input.GetKey(KeyCode.LeftShift) || Input.GetKey(KeyCode.RightShift)) && jumpable)
         {
-
             maxSpeed = Run_maxSpeed;
             moveSpeed = Run_moveSpeed;
         }
@@ -103,72 +146,111 @@ public class PlayerHandler : MonoBehaviour
             maxSpeed = First_maxSpeed;
             moveSpeed = First_moveSpeed;
         }
-        // 공중에서 쉬프트를 누른상태로 바닥에 도착하면 바로 달리게끔하는 기능을 만들고싶으나 아직 바닥에 닿아도 바로 달리지않음. 한번 더 눌러야함. 추가필요할듯.
-
-   
-
-
+    }
+    void Jump()
+    {
+        // 스페이스바를 누르면 점프합니다.
+        if (Input.GetKeyDown(KeyCode.Space) && jumpable)
+        {
+            rig.AddForce(Vector2.up * jumpPower, ForceMode2D.Impulse); 
+        }
+    }
+    void Stomp()
+    {
         // 발구르기 점프중 불가능
-        if ((Input.GetKeyDown(KeyCode.Z)&&isJump)&&isEnded_foot) // isFoot 쿨타임 계산
+        if (Input.GetKeyDown(KeyCode.Z) && jumpable && !isStomping)
         {
-            time_current = time_Max;
             Debug.Log("발구르기 발동");
-            isEnded_foot = false;
-            isFooting = true;
-        } // 일정시간까진 불가능하게하는 메커니즘 필요
-        if((0<time_current)&&isFooting) // 발구르기 행동 진입
-        {
-            time_current -= Time.deltaTime; //쿨타임 계산
-            Debug.Log(time_current);
+            // 발구르기 쿨타임 중이라고 명시
+            isStomping = true;
+            // 발구르기 쿨타임 진행
+            StartCoroutine(CoolingStomp());
         }
-        else if(isFooting)  // 시간 모두 떨어지면
-        {
-            Debug.Log("쿨타임종료");  // 다시 사용가능
-            isFooting = false;
-            isEnded_foot = true;
-        }
-
-        if(!isJump) // 만약 점프가 불가능한 상황 = 공중에 있는 상황이면
-        {
-            ghost.makeGhost = true;// 잔상을 만듭니다.
-        }
-        else if(isJump&&!isWalking ) // 점프가 가능한 상황이고 걷지않는 상황이다
-        {
-            ghost.makeGhost = false; // 분신을 만들지 않습니다.
-        }
-
-
-
-
+    }
+    IEnumerator CoolingStomp()
+    {
+        // 발구르기 쿨타임
+        yield return new WaitForSeconds(cooltime_Stomp);
+        // 위 시간이 지난 후 다시 발구르기 사용 가능한 상태로 변경
+        isStomping = false;
+        Debug.Log("발구르기 쿨타임 종료");
+    }
+    void Shout()
+    {
         // 소리치기 충전 점프중 가능, 게이지 있음.
-        if (Input.GetKeyDown(KeyCode.LeftControl))
+        if (Input.GetKeyDown(KeyCode.LeftControl) && !isShouting)
         {
             Gauge += 1;
         }
-        if (Input.GetKeyUp(KeyCode.LeftControl)) // 키업 하면 소리치기. 게이지 표출
+        // 키업 하면 소리치기. 게이지 표출
+        if (Input.GetKeyUp(KeyCode.LeftControl) && !isShouting) 
         {
-
             Gauge = 0;
+            Debug.Log("소리치기 발동");
+            // 소리치기 쿨타임 중이라고 명시
+            isShouting = true;
+            // 소리치기 쿨타임 진행
+            StartCoroutine(CoolingShout());
         }
     }
+    IEnumerator CoolingShout()
+    {
+        // 소리치기 쿨타임
+        yield return new WaitForSeconds(cooltime_Shout);
+        // 위 시간이 지난 후 다시 소리치기 사용 가능한 상태로 변경
+        isShouting = false;
+        Debug.Log("소리치기 쿨타임 종료");
+    }
+    void MakeGhost()
+    {
+        // 만약 점프가 불가능한 상황 = 공중에 있는 상황이면
+        if (!jumpable)
+        {
+            // 잔상을 만듭니다.
+            ghost.makeGhost = true;
+        }
+        // 점프가 가능한 상황이고 걷지않는 상황이다
+        if (jumpable && !isWalking) 
+        {
+            // 분신을 만들지 않습니다.
+            ghost.makeGhost = false; 
+        }
+    }
+
+    private void Start()
+    {
+        ghost = gameObject.GetComponent<GhostLightHandler>();
+        rig = GetComponent<Rigidbody2D>();
+        moveSpeed = First_moveSpeed;
+        maxSpeed = First_maxSpeed;
+        jumpPower = 14;
+    }
+
+    void Update()
+    {
+        MakeGhost();
+        Walk();
+        Dash();
+        Jump();
+        Stomp();
+        Shout();
+    }
+
     private void OnCollisionEnter2D(Collision2D collision)
     {
         // 땅과 충돌시 점프가능
         if(collision.gameObject.tag == "Ground")
         {
-            isJump = true;
+            jumpable = true;
         }
     }
-
-    // 땅과 떨어질시 점프 불가능.
 
     private void OnCollisionExit2D(Collision2D collision)
     {
-        if(collision.gameObject.tag == "Ground")
+        // 땅과 떨어질시 점프 불가능.
+        if (collision.gameObject.tag == "Ground")
         {
-            isJump = false;
+            jumpable = false;
         }
     }
-
-
 }
